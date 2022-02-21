5f9fc6b5906985522fad51abe271c557d155271d
mbuchet <madison.buchet@gmail>
2022-02-21 11:54:27 +0100

    pouet

--- ft_putunsigned.c
+++ ft_putunsigned.c
@@ -16,9 +16,9 @@ void	ft_putunsigned(unsigned int nb, char base, int *count)
 {
 	if (nb >= BASE)
 		ft_putunsigned(nb / BASE, base, count);
-	if (base == HEXALO)
+	if (base ==  'x')
 		*count += write (1, &STR_HEXA_LOWER[nb % BASE], 1);
-	else if (base == HEXAUP)
+	else if (base == 'X')
 		*count += write (1, &STR_HEXA_UPPER[nb % BASE], 1);
 	else
 		*count += write (1, &STR_DECIMAL[nb % BASE], 1);
